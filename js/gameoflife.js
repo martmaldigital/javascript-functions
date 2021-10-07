947a809e851fbd627ee3c955b967efad788b0fcd
Matthew Aloba <martmaldigital@gmail.com>
2021-10-07 18:47:47 +0100

    functions implemented

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -77,9 +77,18 @@ const calculateNext = (state) => {
 	return result;
 };
 
-const iterate = (state, iterations) => {};
+const iterate = (state, iterations) => {
+	const states = [state];
+	for (let i = 0; i < iterations; i++) {
+		states.push(calculateNext(states[states.lenght - 1]));
+	}
+	return states;
+};
 
-const main = (pattern, iterations) => {};
+const main = (pattern, iterations) => {
+	const results = iterate(startPatterns[pattern], iterations);
+	results.forEach((r) => console.log(printCells(r)));
+};
 
 const startPatterns = {
 	rpentomino: [
